3c697733c6ffcb3c1ec7fd9707ae9f717b7b410b
hikaruk <h.kurosawa@mail.utoronto.ca>
2025-06-03 15:21:38 -0400

    update

--- FOD/FocusOnDepth.py
+++ FOD/FocusOnDepth.py
@@ -69,7 +69,8 @@ class FocusOnDepth(nn.Module):
         self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, emb_dim))
 
         #Transformer
-        encoder_layer = nn.TransformerEncoderLayer(d_model=emb_dim, nhead=12, dropout=transformer_dropout, dim_feedforward=emb_dim*4, norm_first = False)
+        #oriignal - nhead = 12, 3D embed  = 16
+        encoder_layer = nn.TransformerEncoderLayer(d_model=emb_dim, nhead=16, dropout=transformer_dropout, dim_feedforward=emb_dim*4, norm_first = False)
         self.transformer_encoders = nn.TransformerEncoder(encoder_layer, num_layers=num_layers_encoder)
         #self.transformer_encoders = timm.create_model(model_timm, pretrained=True)
         self.type_ = type
